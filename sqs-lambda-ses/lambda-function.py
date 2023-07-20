1e8b0f4c0026225d0f58bf56a23529c1634f5657
gaurav157243 <gaurav157243@gmail.com>
2023-07-20 21:12:45 +0530

    updated the code to read from the env variable

--- sqs-lambda-ses/lambda-function.py
+++ sqs-lambda-ses/lambda-function.py
@@ -7,6 +7,17 @@ from botocore.exceptions import ClientError
 sqs = boto3.client('sqs')
 ses = boto3.client('ses')
 
+def lambda_handler(event, context):
+    sender_email = os.environ.get('EMAIL_ID')
+    recipient_email = os.environ.get('EMAIL_ID')
+    subject = "lambda sqs testing email"
+    
+    for record in event['Records']:
+        print("test")
+        email_body = record["body"]
+        print(str(email_body))
+        send_email(sender_email, recipient_email, subject, email_body)
+
 def send_email(sender_email, recipient_email, subject, body):
     try:
         response = ses.send_email(
@@ -28,15 +39,3 @@ def send_email(sender_email, recipient_email, subject, body):
         print(f"Email sent! Message ID: {response['MessageId']}")
     except ClientError as e:
         print(f"Error sending email: {e.response['Error']['Message']}")
-        
-
-def lambda_handler(event, context):
-    sender_email = "gaurav157243@gmail.com"
-    recipient_email = "shruti.srs@gmail.com"
-    subject = "lambda sqs testing email"
-    
-    for record in event['Records']:
-        print("test")
-        email_body = record["body"]
-        print(str(email_body))
-        send_email(sender_email, recipient_email, subject, email_body)
